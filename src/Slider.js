c50fb036665466b9f5d8f72e78de4bce35e82eae
Richárd Barakovics <113837870+richardbarakovics@users.noreply.github.com>
2022-12-03 19:16:57 +0100

    changed back to img based slider

--- src/Slider.js
+++ src/Slider.js
@@ -1,50 +1,96 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 
-// import screeshot1 from "./images/screenshots/screenshot-1.png";
-// import screeshot2 from "./images/screenshots/screenshot-2.png";
-// import screeshot3 from "./images/screenshots/screenshot-3.png";
-// import screeshot4 from "./images/screenshots/screenshot-4.png";
-// import screeshot5 from "./images/screenshots/screenshot-5.png";
-// import screeshot6 from "./images/screenshots/screenshot-6.png";
-// import screeshot7 from "./images/screenshots/screenshot-7.png";
-// import screeshot8 from "./images/screenshots/screenshot-8.png";
-// import screeshot9 from "./images/screenshots/screenshot-9.png";
-// import screeshot10 from "./images/screenshots/screenshot-10.png";
+import screenshot1 from "./images/screenshots/screenshot-1.png";
+import screenshot2 from "./images/screenshots/screenshot-2.png";
+import screenshot3 from "./images/screenshots/screenshot-3.png";
+import screenshot4 from "./images/screenshots/screenshot-4.png";
+import screenshot5 from "./images/screenshots/screenshot-5.png";
+import screenshot6 from "./images/screenshots/screenshot-6.png";
+import screenshot7 from "./images/screenshots/screenshot-7.png";
+import screenshot8 from "./images/screenshots/screenshot-8.png";
+import screenshot9 from "./images/screenshots/screenshot-9.png";
+import screenshot10 from "./images/screenshots/screenshot-10.png";
 
 
 export default class MultipleItems extends Component {
     render() {
-        const settings = {
+        var settings = {
             dots: true,
-            infinite: true,
+            infinite: false,
             speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 3
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            initialSlide: 0,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         };
         return (
             <div>
                 <h3 className="section--title">Projects</h3>
                 <Slider {...settings}>
-                    <div className="slider--image-1 backgr--img">
+                    <div>
+                        <img src={screenshot1} alt="" />
                     </div>
-                    <div className="slider--image-2 backgr--img">
+                    <div>
+                        <img src={screenshot2} alt="" />
+
                     </div>
-                    <div className="slider--image-3 backgr--img">
+                    <div>
+                        <img src={screenshot3} alt="" />
+
                     </div>
-                    <div className="slider--image-4 backgr--img">
+                    <div>
+                        <img src={screenshot4} alt="" />
+
                     </div>
-                    <div className="slider--image-5 backgr--img">
+                    <div>
+                        <img src={screenshot5} alt="" />
+
                     </div>
-                    <div className="slider--image-6 backgr--img">
+                    <div>
+                        <img src={screenshot6} alt="" />
+
                     </div>
-                    <div className="slider--image-7 backgr--img">
+                    <div>
+                        <img src={screenshot7} alt="" />
+
                     </div>
-                    <div className="slider--image-8 backgr--img">
+                    <div>
+                        <img src={screenshot8} alt="" />
+
                     </div>
-                    <div className="slider--image-9 backgr--img">
+                    <div>
+                        <img src={screenshot9} alt="" />
+
                     </div>
-                    <div className="slider--image-10 backgr--img">
+                    <div>
+                        <img src={screenshot10} alt="" />
+
                     </div>
                 </Slider>
             </div>
